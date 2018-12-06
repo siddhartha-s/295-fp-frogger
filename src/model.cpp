daa2d4a21e6036ed038dbff62fe925382c4d6be6
Sid S <sid@dhcp-10-105-54-254.wireless.northwestern.private>
2018-12-06 12:33:50 -0600

    updated model

--- src/model.cpp
+++ src/model.cpp
@@ -5,6 +5,133 @@
 
 namespace frogger{
 
+    Model::Model()
+    : frog_({{GAMESCREEN_WIDTH/2,GAMESCREEN_HEIGHT-CELLSIZE},Direction::up}),
+    lives_(3),
+    score_(0),
+    state_(State::play),
+    highest_reached_(CELLSIZE),
+    time_elapsed_(0.0)
+    {
+        for(int i = 0; i<8; i++){
+            obstacles_.emplace_back();
+        }
+
+        obstacles_[0].push_back({GAMESCREEN_WIDTH/3,GAMESCREEN_HEIGHT-(2*CELLSIZE)});
+        obstacles_[0].push_back({(2* GAMESCREEN_WIDTH)/3,GAMESCREEN_HEIGHT-(2*CELLSIZE)});
+
+        obstacles_[1].push_back({GAMESCREEN_WIDTH/4,GAMESCREEN_HEIGHT-(3*CELLSIZE)});
+        obstacles_[1].push_back({GAMESCREEN_WIDTH/2,GAMESCREEN_HEIGHT-(3*CELLSIZE)});
+        obstacles_[1].push_back({(3*GAMESCREEN_WIDTH)/4,GAMESCREEN_HEIGHT-(3*CELLSIZE)});
+
+        obstacles_[2].push_back({GAMESCREEN_WIDTH/3,GAMESCREEN_HEIGHT-(4*CELLSIZE)});
+        obstacles_[2].push_back({(2* GAMESCREEN_WIDTH)/3,GAMESCREEN_HEIGHT-(4*CELLSIZE)});
+
+        obstacles_[3].push_back({GAMESCREEN_WIDTH/2,GAMESCREEN_HEIGHT-(5*CELLSIZE)});
+
+        obstacles_[4].push_back({0,3*CELLSIZE});
+        obstacles_[4].push_back({obstacle_dims[4].first,3*CELLSIZE});
+        obstacles_[4].push_back({(GAMESCREEN_WIDTH/2)-obstacle_dims[4].first,3*CELLSIZE});
+        obstacles_[4].push_back({(GAMESCREEN_WIDTH/2),3*CELLSIZE});
+        obstacles_[4].push_back({GAMESCREEN_WIDTH-(2*obstacle_dims[4].first),3*CELLSIZE});
+        obstacles_[4].push_back({GAMESCREEN_WIDTH-obstacle_dims[4].first,3*CELLSIZE});
+
+        obstacles_[5].push_back({0,2*CELLSIZE});
+        obstacles_[5].push_back({GAMESCREEN_WIDTH-obstacle_dims[5].first,2*CELLSIZE});
+
+        obstacles_[6].push_back({GAMESCREEN_WIDTH,CELLSIZE});
+    }
+
+    int Model::calculate_lane() {
+        int lane = frog_.position_.y/CELLSIZE;
+        if(lane == 0) return 100;
+        if(lane > 8 or lane ==4){
+            return -1;
+        }
+        else if(lane >= 5){
+            return 8 - lane;
+        }
+        else if(lane >= 1){
+            return 7-lane;
+        }
+        else{
+            return -1;
+        }
+    }
+
+    void Model::update_score() {
+        if (frog_.position_.y < highest_reached_){
+            score_ +=10;
+            highest_reached_= frog_.position_.y;
+        }
+    }
+
+    bool Model::frog_collide(int lane){
+            for(auto &obstacle: obstacles_[lane]){
+                if(frog_.position_.x<obstacle.x+obstacle_dims[lane].first and
+                frog_.position_.x+CELLSIZE>obstacle.x)
+                {
+
+                    return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool Model::is_move_valid(Direction input) const {
+
+        if(input == Direction::down and frog_.position_.y == GAMESCREEN_HEIGHT-CELLSIZE){
+            return false;
+        }
+        else if(input == Direction::left and frog_.position_.x == 0 and
+        frog_.position_.y >= GAMESCREEN_HEIGHT - 5*CELLSIZE){
+            return false;
+        }
+        else if(input == Direction::right and frog_.position_.x == GAMESCREEN_WIDTH-CELLSIZE and
+        frog_.position_.y >= GAMESCREEN_HEIGHT - 5*CELLSIZE){
+            return false;
+        }
+
+        return true;
+    }
+
+    void Model::update_game_state() {
+        if(lives_==0 or time_elapsed_ == GAME_TIME){
+            state_= State::gameover;
+        }
+    }
+
+    void Model::move_frog(Direction motion) {
+        if(motion == Direction::up and is_move_valid(motion)){
+            frog_.position_.y -= CELLSIZE;
+        }
+        else if(motion == Direction::down and is_move_valid(motion)){
+            frog_.position_.y += CELLSIZE;
+        }
+        else if(motion == Direction::left and is_move_valid(motion)){
+            frog_.position_.x -= CELLSIZE;
+        }
+        else if(motion == Direction::right and is_move_valid(motion)){
+            frog_.position_.x += CELLSIZE;
+        }
+
+        int lane = calculate_lane();
+
+        bool check = false;
+        if(lane >= 0 and lane < 4){
+            check = frog_collide(lane);
+        }
+        else if(lane < 7 and lane >= 4){
+            check = frog_drown(lane);
+        }
+        if(check){
+            --lives_;
+            frog_ = {{GAMESCREEN_WIDTH/2,GAMESCREEN_HEIGHT-CELLSIZE},Direction::up};
+        }
+
+        update_game_state();
+    }
 }
 
 
--- src/model.h
+++ src/model.h
@@ -4,96 +4,98 @@
 #include<string>
 #include<array>
 
-namespace frogger {
+namespace frogger{
 
-    const int total_lane_length = 64;
-    const int lane_on_screen = 18;
-    int game_time = 60;
-
-
-    //how we decide to represent the state of the game - whether the game is in progress or over.
+    //each cell is 25x25 px. the screen is 14 cells wide and 13 cells tall, 2 cel extra rows below for
+    //to show score and lives and time.
+    //the time the game runs for
+    const int GAME_TIME = 60;
+    //the size of each cell of the game
+    const int CELLSIZE = 25;
+    //gamescreen width
+    const int GAMESCREEN_WIDTH = 14*CELLSIZE;
+    //gamescreen height
+    const int GAMESCREEN_HEIGHT = 10*CELLSIZE;
+    //velocity for object in each lane
+    const std::vector<float> lane_velocity = {2.0f, -4.0f,3.0f,-3.0f,2.0f,3.0f,-3.0f};
+    //dimensions for object in each lane
+    const std::vector<std::pair<int,int>> obstacle_dims = {{25,25},{25,25},{25,25},{25,25},{25,25},{25,25},{25,25}};
+    //Represents the state of the game - whether the game is in progress or over.
     enum class State{
-        play, over
+        play, win, gameover
     };
-
     //used to determine the direction the frog is facing
     enum class Direction{
         up, down, left, right
     };
 
-    using Coordinate = double;
 
     /// Models an instance of the classic Atari Frogger game.
 
     class Model {
 
     public:
-        using Rectangle = ge211::Basic_rectangle<Coordinate>;
+
+        using Coordinate = double;
 
         explicit Model();
 
         int lives() const { return lives_; }
 
-        int times_up() const { return time_elapsed_ - game_time <= 0; }
+        int times_up() const { return time_elapsed_ - GAME_TIME <= 0; }
 
-        bool frog_collide();
 
-        bool frog_drown();
+        //checks whether the flog has collided in `lane`
+        bool frog_collide(int);
 
-        void move_frog(Coordinate dx, Coordinate dy);
+        //checks whether the frog has drowned in the river, in the river section
+        bool frog_drown(int);
+
+        void move_frog(Direction);
 
         //Updates game as time passes.
         void update(double const dt);
 
-        Rectangle get_screen() { return screen_; }
-
-
     private:
 
 
-        //A lane represents one row of the screen, represented by the pair
-        using Lane = std::pair<Coordinate, std::array<size_t, total_lane_length>>;
-
-        /// We represent the entire screen in terms of a vector of lanes.
-        /// The screen has seven lanes. `Lane.first` represents the velocity in which the lane moves
-        /// A positive velocity means the lane moves from left to right and vice versa.
-        /// The `Lane.second` vector contains information on which spots are safe and which spots are unsafe for the
-        /// frog to move to.
-        std::vector<Lane> vecLanes;
-
-        Rectangle const screen_;
-
+        //A vector of vectors of positions of the "obstacles" ("obstacles" are vehicles in the road section, which need
+        //to be avoided and logs and floating objects in the river section which need to be landed on.
+        std::vector<std::vector<ge211::Position>> obstacles_;
 
         struct Frog {
-            std::pair<Coordinate, Coordinate> position;
-            Direction direction;
+            ge211::Position position_;
+            Direction direction_;
         };
 
-        Frog frog;
+        Frog frog_;
 
         //State of the game - in progress or over.
         State state_ = State::play;
 
         int lives_; //no of lives left
-        int score_;
+        int score_; //score
+        int highest_reached_; //maintains the furthest lane the frog has reached; required to update the score.
 
         float time_elapsed_; //time elapsed since start of the game
 
-        /// Updates `winner_` and `turn_` at the end of turn_'s turn.
-        /// **PRECONDITION** `col_no` and `row_no` is valid and where the last move
-        /// was played.
-        void update_lives_game_state();
+        /// Updates `lives_`, `score_` and `state_`
+        void update_game_state();
 
         //Updates `score_` based on player move;
         void update_score();
 
         //Updates all `Lane`s in `vecLanes` as time passes
-        void lane_update(double const dt);
+        void update_obstacles_();
 
-        void time_update(double const dt);
+        //Updates time_elapsed
+        void update_time_elapsed_();
+
+        //computes which lane the frog is in; to be used to make checking for collisions easier
+        int calculate_lane();
 
         /// Returns whether the coordinate is within bounds of screen.
-        bool is_within_bounds(Coordinate dx, Coordinate dy) const;
+        bool is_move_valid(Direction) const;
     };
 
 }
